f3f6e057fd0448ad2aa3960b7c08fbd27487539c
tangbin224 <18814888121@163.com>
2019-04-24 19:17:44 +0800

    a

--- config/config.default.js
+++ config/config.default.js
@@ -10,17 +10,19 @@ module.exports = appInfo => {
    * built-in config
    * @type {Egg.EggAppConfig}
    **/
-  const config = exports = {
-    csrf: {
-      enable: false,
-    },
-  };
+  const config = exports = {};
 
   // use for cookie sign key, should change to your own and keep security
   config.keys = appInfo.name + '_1556088659992_936';
   config.appSecret = '06e09f06712d9b2acfc115f6b3c080cc';
   // add your middleware config here
   config.middleware = [];
+
+  config.security = {
+    csrf: {
+      enable: false,
+    },
+  };
 
   // add your user config here
   const userConfig = {
