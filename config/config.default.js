24eda60c0cc3d46f2342620cb0bce001237f88b1
tangbin224 <18814888121@163.com>
2019-04-24 19:06:07 +0800

    a

--- config/config.default.js
+++ config/config.default.js
@@ -10,7 +10,11 @@ module.exports = appInfo => {
    * built-in config
    * @type {Egg.EggAppConfig}
    **/
-  const config = exports = {};
+  const config = exports = {
+    csrf: {
+      enable: false,
+    },
+  };
 
   // use for cookie sign key, should change to your own and keep security
   config.keys = appInfo.name + '_1556088659992_936';
