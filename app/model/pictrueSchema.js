b57c77d623ea580f4461175c10e0cd88b2d21461
tangbin224 <18814888121@163.com>
2019-04-29 20:22:06 +0800

    a

--- app/model/pictrueSchema.js
+++ app/model/pictrueSchema.js
@@ -2,7 +2,6 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 module.exports = new Schema({
-  _id: Schema.Types.ObjectId,
   name: String,
   url: String,
   messageType: String,
