d84d3c7d22f135aad63838393708dea858a61b0f
tangbin224 <18814888121@163.com>
2019-05-04 23:11:08 +0800

    n

--- app/middleware/onlyWX.js
+++ app/middleware/onlyWX.js
@@ -5,7 +5,7 @@ module.exports = () => {
   return async function onlyWX(ctx, next) {
     await next();
     const header = ctx.request.header;
-    const ua = header['user-agent'];
+    const ua = header['user-agent'].toLowerCase();
     const isWX = /.*micromessenger.*/.test(ua);
     if (!isWX) {
       await ctx.render('error.htm', {});
